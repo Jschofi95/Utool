e4639408bbdb71fdf6c839cd7713dbc10ec82602
dylanthebrowns <dtbrowns10@gmail.ua.edu>
2023-02-28 14:54:01 -0600

    added card to homepage with image

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -14,10 +14,43 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: Column(
-        children: const [
-          Text("This is the home page where all of the listings will be posted"),
-        ],
+      body: const MyCards(),
+    );
+  }
+}
+
+class MyCards extends StatelessWidget {
+  const MyCards({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Image.asset('assets/woodchipper_placeholder.png'),
+              title: Text('Woodchipper'),
+              subtitle: Text('\$100 per day'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('Rent'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('Message'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
