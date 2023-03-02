2e4c9177c140ae187bc3b4ca423df59b1edff41d
dylanthebrowns <dtbrowns10@gmail.ua.edu>
2023-03-01 19:18:23 -0600

    added basic styling in main. will change

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'My App',
       home: LoginPage(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        fontFamily: 'Helvelica',
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 36.0),
+          bodyMedium: TextStyle(fontSize: 18.0),
+        ),
+      ),
     );
   }
 }
