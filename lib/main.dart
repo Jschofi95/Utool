a2ecdfc7b71c0158f31e9fd8c1649595231b34d0
Aaron Schofield <jamesaschof@gmail.com>
2023-03-21 14:36:04 -0500

    Added preset theme options

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:utool/homepage/homepage.dart';
 import 'package:utool/init.dart';
 import 'package:utool/login/login.dart';
 import 'package:utool/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart'; //
+import 'package:firebase_core/firebase_core.dart';
+import 'package:utool/themes/theme_layouts.dart';
 //import 'firebase_options.dart'; // Generated file
 
 void main() {
@@ -27,17 +28,7 @@ class MyApp extends StatelessWidget {
           }
         },
       ),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
-        fontFamily: 'Helvelica',
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(fontSize: 36.0),
-          bodyMedium: TextStyle(fontSize: 18.0),
-        ),
-      ),
+      theme: darkTheme1(),
     );
   }
 }
