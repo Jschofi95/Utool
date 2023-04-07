5df739559cd17def90d9bc88dc9a8180bb21cbf6
Aaron Schofield <jamesaschof@gmail.com>
2023-04-07 13:39:27 -0500

    merge from main

---
+++ lib/post/post.dart
@@ -0,0 +1,295 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:utool/homepage/homepage.dart';
+import 'package:utool/item/item_data.dart';
+import 'successfulpost.dart';
+import 'package:utool/enums/enums.dart';
+
+class Post extends StatefulWidget {
+  const Post({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _Post();
+}
+
+class _Post extends State<Post> {
+  final idController = TextEditingController();
+  final typeController = TextEditingController();
+  final brandController = TextEditingController();
+  final modelController = TextEditingController();
+  final priceController = TextEditingController();
+  final rentPriceIntervalController = TextEditingController();
+  final conditionController = TextEditingController();
+  final useTypeController = TextEditingController();
+  final addressLine1Controller = TextEditingController();
+  final addressLine2Controller = TextEditingController();
+  final cityController = TextEditingController();
+  final stateController = TextEditingController();
+  final zipCodeController = TextEditingController();
+  final deliveryFeeController = TextEditingController();
+  final hoursController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final imgLinksController = TextEditingController();
+
+  Map<String, dynamic> item = {
+    'id': '1b',
+    'type': '',
+    'brand': '',
+    'model': '',
+    'price': 0.0,
+    'rentPriceInterval': '',
+    'condition': '',
+    'useType': '',
+    'addressLine1': '',
+    'addressLine2': '',
+    'city': '',
+    'state': '',
+    'zipCode': 0,
+    'deliveryFee': 0.0,
+    'hours': 0.0,
+    'description': '',
+    'imgLinks': ['assets/woodchipper_placeholder.png'],
+  };
+
+  void buildItem() {
+    item['type'] = typeController.text;
+    item['brand'] = brandController.text;
+    item['model'] = modelController.text;
+    item['price'] = dynToDouble(priceController.text);
+    item['rentPriceInterval'] = rentPriceIntervalController.text;
+    item['condition'] = conditionController.text;
+    item['useType'] = useTypeController.text;
+    item['addressLine1'] = addressLine1Controller.text;
+    item['addressLine2'] = addressLine2Controller.text;
+    item['city'] = cityController.text;
+    item['state'] = stateController.text;
+    item['zipCode'] = dynToDouble(zipCodeController.text);
+    item['deliveryFee'] = dynToDouble(deliveryFeeController.text);
+    item['hours'] = dynToDouble(hoursController.text);
+    item['description'] = descriptionController.text;
+    item['imgLinks'] = imgLinksController.text;
+  }
+
+  // Dynamic to double
+  double dynToDouble(String data) {
+    double price;
+    if (double.tryParse(data) != null) {
+      price = double.parse(data);
+    } else {
+      // Handle the case where the price is not a valid numeric value
+      // For example, you can set the price to a default value or show an error message
+      price = 0.0;
+    }
+    return price;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New post'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              //type
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Tool Type',
+                    hintText: 'Enter type'),
+              ),
+            ),
+
+            //imageLink
+            // Padding(
+            //   padding: EdgeInsets.symmetric(horizontal: 10),
+            //   child: TextField(
+            //     controller: imageController,
+            //     decoration: InputDecoration(
+            //         border: OutlineInputBorder(),
+            //         labelText: 'ImageLink',
+            //         hintText: 'Enter image'),
+            //   ),
+            // ),
+
+            //brand
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: brandController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Brand',
+                    hintText: 'Enter brand'),
+              ),
+            ),
+
+            //price
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: priceController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Price',
+                    hintText: 'Enter price // per day'),
+              ),
+            ),
+
+            //rentPriceInterval
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: rentPriceIntervalController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Rent Price Interval',
+                    hintText: 'Enter price Interval'),
+              ),
+            ),
+
+            //condition
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: conditionController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Condition',
+                    hintText: 'Enter % of new'),
+              ),
+            ),
+
+            //useType
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: useTypeController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Use Type',
+                    hintText: 'Enter use type'),
+              ),
+            ),
+
+            //address
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: addressLine1Controller,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Address line 1',
+                    hintText: 'Enter address'),
+              ),
+            ),
+
+            //address2
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: addressLine2Controller,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Address line 2',
+                    hintText: 'Enter address'),
+              ),
+            ),
+            //city
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: cityController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'City',
+                    hintText: 'Enter City'),
+              ),
+            ),
+
+            //state
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: stateController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'State',
+                    hintText: 'Enter State'),
+              ),
+            ),
+
+            //zipCode
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: zipCodeController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Zip Code',
+                    hintText: 'Enter zip code'),
+              ),
+            ),
+
+            //deliveryFee
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: deliveryFeeController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Would you pay deliveryFee',
+                    hintText: 'Enter Y for yes, N for no'),
+              ),
+            ),
+
+            //hours
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: hoursController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'How long would you like to rent',
+                    hintText: 'Enter time'),
+              ),
+            ),
+
+            //description
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Discription',
+                    hintText: 'Enter discription'),
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+                buildItem();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => SuccessfulPost(
+                              item: item,
+                            )));
+              },
+              child: const Text(
+                'Post',
+                style: TextStyle(color: Colors.red, fontSize: 25),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/post/successfulpost.dart
@@ -0,0 +1,147 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+import 'package:utool/homepage/homepage.dart';
+import 'package:utool/item/item_data.dart';
+
+class SuccessfulPost extends StatelessWidget {
+  final Map<String, dynamic> item;
+
+  const SuccessfulPost({Key? key, required this.item}) : super(key: key);
+
+  TextStyle defaultHintStyle() {
+    return TextStyle(
+        color: Colors.white.withOpacity(0.5),
+        fontWeight: FontWeight.bold,
+        fontSize: 12.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Review'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          //width: double.infinity,
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Type: ${item['type']}",
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Brand: ${item['brand']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Price: ${item['price']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Rent Interval: ${item['rentPriceInterval']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Condition: ${item['condition']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Use type: ${item['useType']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Address line 1: ${item['addressLine1']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Address line 2: ${item['addressLine2']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("City: ${item['city']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("State: ${item['state']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Zip code: ${item['zipCode']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Delivery Fee: ${item['deliveryFee']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Hours: ${item['hours']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text("Description: ${item['description']}"),
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(bottom: 8.0),
+                alignment: Alignment.center,
+                child: Text(
+                  "Is this information correct?",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right: 4.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Submit'),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 4.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Edit'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
